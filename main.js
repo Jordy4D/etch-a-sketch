7ce07f96412c7f24bef495a2cb1bd1000cb10da9
Jordan Adams <jsadams27@gmail.com>
2016-03-26 18:09:38 -0500

    added additional functionality, reset button

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 
-		for (var x = 0; x < 16; x++) {
-			$('#container').append("<div class='grid'></div>")
+		var gridAmount = window.prompt("From 1 to 64, what is your grid's parameters (00 x 00)?");
+		var gridSize;
+
+		if (gridAmount === null || gridAmount === undefined || gridAmount > 64 || isNaN(gridAmount)){
+			var gridSize = 16;
+		} else {
+			var gridSize = gridAmount;
 		};
-		for (var x = 0; x < 16; x++) {
+
+		for (var x = 0; x < gridSize; x++) {
+			$('#container').append("<div class='grid'></div>");
+			$('.grid').height(800 / gridSize);
+		};
+		for (var x = 0; x < gridSize; x++) {
 			$('.grid').append("<div class='row'></div>")
+			$('.row').width(800 / gridSize);
+			$('.row').height(800 / gridSize);
 		};
 
 		
@@ -14,9 +26,11 @@ $(document).ready(function(){
 	    		$(this).addClass( "drawing" );
 	  	});
 		
-		$('button').click(function(){
+		$('#reset-grid').click(function(){
 			$('.drawing').removeClass("drawing")
 		});
-	
+		$('#reset-squares').click(function(){
+			location.reload(true);
+		});
 	});
 
